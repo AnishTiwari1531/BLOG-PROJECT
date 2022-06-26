c3a026f8116188414aeed10d65acc6873cb819f8
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-06-26 23:57:06 +0530

    Project_1_MINI_BLOGGING_SITE

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,9 +6,13 @@ const commonMiddleware = require("../middleware/auth")
 
 //---------------------------------------------------------------//
 
+//...........................Author..............................//
+
 router.post("/authors", authorController.createAuthor)
 
 router.post("/login", authorController.login)
+
+//...........................Blog...............................//
 
 router.post("/blogs",commonMiddleware.tokenChecker, blogController.createBlog)
 
@@ -16,11 +20,12 @@ router.get("/blogs", commonMiddleware.tokenChecker, blogController.getBlogs)
 
 router.put("/blogs/:blogId", commonMiddleware.tokenChecker, blogController.update)
 
+//...........................Delete.............................//
+
 router.delete("/blogs/:blogId", commonMiddleware.tokenChecker, blogController.deleteByBlogId)
 
 router.delete("/blogs", commonMiddleware.tokenChecker, blogController.deleteByQuery)
 
+//............................................................//
+
 module.exports = router;
-
-
-//---------------------------------------------------------------//
